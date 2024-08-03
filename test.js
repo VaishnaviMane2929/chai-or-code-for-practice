2f6bf06838a66737b732b9570d034df9a6efb1db
Vaishnavi Mane 2929 <vaishnavimane991@gmail.com>
2024-08-03 13:59:34 +0530

    js practice

---
+++ test.js
@@ -0,0 +1,19 @@
+// const accountId = 123456
+// let accountEmail = "vaishnavi@gmail.com"
+// var accountPassword = "09876"
+// accountCity = "paithan"
+// let accountState;
+
+//accountId = 2
+
+// accountEmail = "hgdd@vc.com"
+// accountPassword = "176383"
+// accountCity = "nagpur"
+
+// console.log(accountId);
+
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+//alert(3+3)
+
+console.log(typeof undefined);
