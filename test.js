7c76ebd3ea46dc0ca63d6227d11f29770ff5b2fc
Vaishnavi Mane 2929 <vaishnavimane991@gmail.com>
2024-08-20 22:32:14 +0530

    string concatenate

--- test.js
+++ test.js
@@ -94,7 +94,10 @@ const gameName = new String('vaishumane')
 console.log(gameName[0]);
 console.log(gameName.__proto__);
 console.log(gameName.length);
-console.log(gameName.charAt(3));
+console.log(gameName.indexOf('i'));
+const newString = gameName.substring(0,4)
+console.log(newString);
+
 
 
 
