5afe821c64246a938f4b8e464cd39f2580aa0f38
Vaishnavi Mane 2929 <vaishnavimane991@gmail.com>
2024-08-21 22:14:54 +0530

    string value

--- test.js
+++ test.js
@@ -109,8 +109,14 @@ console.log(url.replace('%29', '-'));
 console.log(url.includes('function'));
 
 const score = 400
+console.log(score);
+
 const balance = new Number(100)
 console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
 
 
 
